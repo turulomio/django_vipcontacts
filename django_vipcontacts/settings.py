78ed6fcad3ccd38818626c0840b17de5fd2bc422
turulomio <turulomio@yahoo.es>
2021-03-27 21:09:52 +0100

    Only token authentication

--- django_vipcontacts/settings.py
+++ django_vipcontacts/settings.py
@@ -28,6 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        #'rest_framework.authentication.BasicAuthentication', 
+        'rest_framework.authentication.TokenAuthentication', 
+    ]
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
