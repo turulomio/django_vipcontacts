badeed9abfafe5b08fe4979d6139d439d6181f91
turulomio <turulomio@yahoo.es>
2021-04-04 10:08:51 +0200

    Changing cors

--- django_vipcontacts/settings.py
+++ django_vipcontacts/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = "https://localhost", "http://localhost:8002", 
+CORS_ORIGIN_WHITELIST = "https://localhost", "http://localhost:8003", 
 
 ROOT_URLCONF = 'django_vipcontacts.urls'
 
