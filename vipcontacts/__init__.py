4b498341520b3d9b383ef35b784d8a7807e4f86c
turulomio <turulomio@yahoo.es>
2021-04-19 13:00:13 +0200

    Adding version and improving procedure

---
+++ vipcontacts/__init__.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+__version__="0.2.0"
+__versiondatetime__= datetime(2021, 4, 19, 8, 13)
+__versiondate__=__versiondatetime__.date()
--- vipcontacts/management/commands/procedure.py
+++ vipcontacts/management/commands/procedure.py
@@ -1,21 +1,29 @@
 from django.core.management.base import BaseCommand
-from books.__init__ import __version__
+from vipcontacts.__init__ import __version__
 
 class Command(BaseCommand):
     help = 'New release procedure'
 
     def handle(self, *args, **options):
-        print("""To release a new version:
-  * Change version and version date in books.__init__.py
+        print("Updating versions of vipcontacts frontend project")
+
+        print(f"""To release a new version:
+DJANGO_VIPCONTACTS
+  * Change version and version date in vipcontacts.__init__.py
   * Add release changelog en README.md
   * python manage.py makemessages
   * linguist
   * python manage.py compilemessages
   * python manage.py doxygen
-  * git commit -a -m 'dj_books-{}'
+  * git commit -a -m 'django_vipcontacts-{__version__}'
   * git push
-  * Hacer un nuevo tag en GitHub
-  * Crea un nuevo ebuild de Gentoo con la nueva versión
-  * Subelo al repositorio del portage
-""".format(__version__))
+  * Hacer un nuevo tag en GitHub de django_vipcontacts
+
+VIPCONTACTS
+  * Cambiar a vipcontacts project
+  * Add release changelog in README.md
+  * git commit -a -m 'vipcontacts-{__version__}'
+  * git push
+  * Hacer un nuevo tag en GitHub de vipcontacts
+""")
 
